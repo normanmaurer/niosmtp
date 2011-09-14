959177cc320f88b12e3c2ff66b13a494de2f331c
norman <norman@apache.org>
2011-09-14 15:47:43 +0200

    cleanup

--- src/main/java/me/normanmaurer/niosmtp/impl/internal/SMTPStartTLSClientHandler.java
+++ src/main/java/me/normanmaurer/niosmtp/impl/internal/SMTPStartTLSClientHandler.java
@@ -3,12 +3,19 @@ package me.normanmaurer.niosmtp.impl.internal;
 import java.io.InputStream;
 import java.util.LinkedList;
 
+import javax.net.ssl.SSLEngine;
+
 import me.normanmaurer.niosmtp.SMTPClientConfig;
 
 public class SMTPStartTLSClientHandler extends SMTPClientHandler{
 
-    public SMTPStartTLSClientHandler(SMTPClientFutureImpl future, String mailFrom, LinkedList<String> recipients, InputStream msg, SMTPClientConfig config) {
+    private SSLEngine engine;
+    private boolean dependOnStartTLS;
+
+    public SMTPStartTLSClientHandler(SMTPClientFutureImpl future, String mailFrom, LinkedList<String> recipients, InputStream msg, SMTPClientConfig config, boolean dependOnStartTLS, SSLEngine engine) {
         super(future, mailFrom, recipients, msg, config);
+        this.dependOnStartTLS = dependOnStartTLS;
+        this.engine = engine;
     }
 
 }
